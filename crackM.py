b65942d55e6ea94647c0602d986a05a78172a1d8
webbi <93513980+webbigamer@users.noreply.github.com>
2023-02-28 23:50:03 +0330

    Update crackM.py

--- crackM.py
+++ crackM.py
@@ -22,4 +22,53 @@ for i in range(0, len(wlistlines)):
         print("hash cracked: "+wlistlines[i].replace("\n",""))
         exit()
 print("hash was not cracked try using another wordlist")
-print("exited")
+print("2/3 tries")
+
+print('''created by webbi
+                      _    __  __ 
+                     | |  |  \/  |
+   ___ _ __ __ _  ___| | _| \  / |
+  / __| '__/ _` |/ __| |/ / |\/| |
+ | (__| | | (_| | (__|   <| |  | |
+  \___|_|  \__,_|\___|_|\_\_|  |_|
+                                  
+                                  
+''')
+
+wlist=input("enter wordlist directory: ")
+hash2crack=input("enter hash [MD5]: ")
+
+wlistlines=open(wlist, "r").readlines()
+
+for i in range(0, len(wlistlines)):
+    hash2comp=hashlib.md5(wlistlines[i].replace("\n","").encode()).hexdigest()
+    if hash2crack == hash2comp:
+        print("hash cracked: "+wlistlines[i].replace("\n",""))
+        exit()
+print("hash was not cracked try using another wordlist")
+print("1/3 tries")
+
+print('''created by webbi
+                      _    __  __ 
+                     | |  |  \/  |
+   ___ _ __ __ _  ___| | _| \  / |
+  / __| '__/ _` |/ __| |/ / |\/| |
+ | (__| | | (_| | (__|   <| |  | |
+  \___|_|  \__,_|\___|_|\_\_|  |_|
+                                  
+                                  
+''')
+
+wlist=input("enter wordlist directory: ")
+hash2crack=input("enter hash [MD5]: ")
+
+wlistlines=open(wlist, "r").readlines()
+
+for i in range(0, len(wlistlines)):
+    hash2comp=hashlib.md5(wlistlines[i].replace("\n","").encode()).hexdigest()
+    if hash2crack == hash2comp:
+        print("hash cracked: "+wlistlines[i].replace("\n",""))
+        exit()
+print("hash was not cracked try using another wordlist")
+print("failed 3 times try restarting the script")
+exit()
